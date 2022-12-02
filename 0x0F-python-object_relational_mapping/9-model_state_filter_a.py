4af63b7fe55d071d33fa02834bcf0c2ddd0f8adb
brian yobra <briannjoroge89@gmail.com>
2022-12-02 15:02:50 +0300

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that lists all `State` objects that contain
-the letter `a` from the database `hbtn_0e_6_usa`.
 
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-"""
 
 import sys
 from sqlalchemy import (create_engine)
