04557bbc8146dc25b6a7c88adaa9559020209b32
brian yobra <briannjoroge89@gmail.com>
2022-12-02 15:00:57 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""
-Script that prints all `City` objects from the database `hbtn_0e_14_usa`.
-
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-"""
 
 import sys
 from sqlalchemy import (create_engine)
