38106d121f640a60ccb6524b2cea2df34963ca71
brian yobra <briannjoroge89@gmail.com>
2022-12-02 14:59:39 +0300

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that lists all `State` objects, and corresponding
-`City` objects, contained in the database `hbtn_0e_101_usa`.
 
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-"""
 
 import sys
 from sqlalchemy import (create_engine)
