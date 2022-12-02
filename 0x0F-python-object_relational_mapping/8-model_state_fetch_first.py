b40db07d414fb572d3aaf8745011b8e30b5d9fdd
brian yobra <briannjoroge89@gmail.com>
2022-12-02 15:02:33 +0300

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that prints the first `State` object from the database `hbtn_0e_6_usa`.
 
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-"""
 
 import sys
 from sqlalchemy import (create_engine)
